9a3fab2901159cf01d697ac1281580ee2435eb1a
Josafat Martinez <josafatmartinez@users.noreply.github.com>
2025-08-10 20:21:15 -0600

    refactor(routes): update application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'listings',
+    loadComponent: () => import('./listings/wizard/listing-wizard/listing-wizard').then(c => c.ListingWizard),
+    title: 'Crear publicación',
+  },
+  {
+    path: '**', redirectTo: 'listings'
+  }
+];
