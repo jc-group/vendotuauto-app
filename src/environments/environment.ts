909706c4aae157c5b544849e8f9f85b27a3b96e5
Josafat Martinez <josafatmartinez@users.noreply.github.com>
2025-09-07 21:15:28 -0600

    :green_book: docs(ci): include Angular environments in Git for CI builds
    Remove environments ignore and commit environment files so Vercel can resolve imports during build. Ensure only non-secret, public keys are stored here.

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  supabaseUrl: 'https://cnccwhzremmanxqmhjil.supabase.co',
+  supabaseAnonKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuY2N3aHpyZW1tYW54cW1oamlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzMTE3MTQsImV4cCI6MjA2OTg4NzcxNH0.Ffk_S3RWCP_tKPY7LksCTMh8VIc26IzNsnQgZOavEws',
+};
